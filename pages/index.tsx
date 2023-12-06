63c09c42833b3f6fbe875ed598b95a72da4b5079
unknown <kentaro.keren@gmail.com>
2023-12-07 04:25:00 +0700

    didnt fix anything

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,25 @@
-import Head from 'next/head'
-import DefaultBackground from 'components/bars/layouts/DefaultBackground'
+import Head from 'next/head';
+import Image from 'next/image'
 
-
-export default function Login() {
+export default function index() {
   return (
     <>
-
     <Head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-  <link rel="shortcut icon" type="image/png" href="../public/logo.png" />
+  <link rel="shortcut icon" type="image/png" href="/logo.png" />
   <title>Landing Page</title>
     </Head> 
 
     <body id="iau9">
       <div id="i6po" className="gjs-grid-row">
-        <div id="iqjl" className="gjs-grid-column">
-          <img src="../public/logo.png" alt="" id="il1b4"/>
+        <div className="gjs-grid-column.feature-item">
+        <Image
+        src="/logo.png"
+        alt="Relow"
+        width={150}
+        height={150}
+        style={{ width: 'auto' }}
+        />
           </div>
           <div id="id7x" className="gjs-grid-column">
             <a href="#testimonial-section" id="i6zal" className="gjs-link">Perangkat</a>
@@ -30,52 +34,95 @@ export default function Login() {
                 </a>
                 </div>
                 </div>
-                <div id="irz7b" className="gjs-grid-row">
-                  <div id="iycz3" className="gjs-grid-column">
-                    <div id="iip4n" className="gjs-image-box">
-                      </div>
-                      <h1 id="irjua" className="gjs-heading">
-                        Ease <span id="if28h">your day,</span><br/>Comfort <span id="i5aak">your life</span>
-                        </h1><div id="itndw" className="text-main-content">
-                          "Clean your shoe because you deserve it." -Tsun Zu</div>
-                          </div>
-                          </div>
-                          <div id="ioyxg" className="gjs-grid-row">
-                            <div id="i8w4i" className="gjs-grid-column">
-                              <h2 id="iiacm" className="gjs-heading">
-                                <span id="ip3bh"><span id="ii86ai">
-                                  <b id="inp4tc">Karena Kami</b></span><br/>Menjadi </span>#LebihMudah</h2>
-                                  <div id="izhbz">Gunakan fitur mudah dengan langkah yang sederhana dari aplikasi Relow<br/>Sekarang kamu tidak bingung kapan dan dimana sepatu anda dicuci</div>
-                                  </div></div>
-                                  <div id="iz29ek" className="gjs-grid-row">
-                                    <div id="iy4ik5" className="gjs-grid-column">
-                                      <img id="iffiq7" src="../public/house_solid.svg"/>
-                                      <div id="i7nidf" className="text-main-content">Buka aplikasi Relow</div>
-                                      </div><div id="iziehg" className="gjs-grid-column">
-                                        <img src="../public/cart_shopping_solid.svg" id="ite65n"/>
-                                        <div id="is8k9o" className="text-main-content">Pesan order baru</div>
-                                        </div><div id="iwtror" className="gjs-grid-column">
-                                          <img id="ignb4m" src="../public/list_solid.svg"/>
-                                          <div id="i2madm" className="text-main-content">Cek status sepatu</div>
-                                          </div></div><div id="iwaw4c" className="gjs-grid-row">
-                                            <div id="iauvhi" className="gjs-grid-column"><h2 id="ibx04w" className="gjs-heading">
-                                              <span id="in98za">Pengalaman mereka</span><br/>bersama kami</h2>
-                                              <div id="i6tlq1" className="text-main-content">Yuk kita lihat pengalaman dari pengguna jasa kita yang sudah merasakan seberapa mudah<br/>
-                                              mencuci sepatu mereka dengan jasa Relow. Simak nih beberapa testimoni dari rekan kita!<br/></div>
-                                              <div id="ijgzmd" className="gjs-grid-row"><div id="irlkry" className="gjs-grid-column">
-                                                <img src="../public/yodyod.png" id="imdy45"/><div id="ilsw14" className="text-main-content">
-                                                  <b id="i2qym3">Yodi</b></div><div id="i9u6o4" className="text-main-content">Ini sangat trendy apalagi<br/>
-                                                  buat anak muda yang selalu mantap fashionnya</div></div><div id="is2klr" className="gjs-grid-column">
-                                                    <img src="../public/zada.jpg" id="ij838p"/><div id="igfcaj" className="text-main-content">
-                                                      <b id="i6y44y">Zada</b></div><div id="iycjvv" className="text-main-content">
-                                                        Aku sempet bingung kalau<br/>mau cuci sepatu dimana<br/>untung ada aplikasi Relow</div></div>
-                                                        <div id="i8itcw" className="gjs-grid-column"><img src="../public/lao.jpg" id="i5493j"/>
-                                                        <div id="i3a3yz" className="text-main-content"><b id="ine986">Laode</b></div>
-                                                        <div id="ijmg5d" className="text-main-content">Alhamdulillah setelah Relow saya merasa sangat percaya diri</div></div></div></div></div>
-                                                        <div id="iv3be5" className="gjs-grid-row"><div id="ilriti" className="gjs-grid-column">
-                                                          <div id="id7ol" className="gjs-text-blue">Terms &amp; Conditions</div>
-                                                          <a id="ipjdtn" href="https://grapesjs.com/" className="gjs-link">2023 Relow - All your right are belong to us</a></div></div>
-                                                          </body>
+  <div id="irz7b" className="gjs-grid-row">
+  <div id="iycz3" className="gjs-grid-column">
+    <div id="iip4n" className="gjs-image-box">
+      </div>
+      <h1 id="irjua" className="gjs-heading">
+        Ease <span id="if28h">your day,</span><br/>Comfort <span id="i5aak">your life</span>
+        </h1><div id="itndw" className="text-main-content">
+          "Clean your shoe because you deserve it." -Tsun Zu</div>
+          </div>
+          </div>
+          <div id="ioyxg" className="gjs-grid-row">
+            <div id="i8w4i" className="gjs-grid-column">
+              <h2 id="iiacm" className="gjs-heading">
+                <span id="ip3bh"><span id="ii86ai">
+    <b id="inp4tc">Karena Kami</b></span><br/>Menjadi </span>#LebihMudah</h2>
+    <div id="izhbz">Gunakan fitur mudah dengan langkah yang sederhana dari aplikasi Relow<br/>Sekarang kamu tidak bingung kapan dan dimana sepatu anda dicuci</div>
+    </div></div>
+    <div id="iz29ek" className="gjs-grid-row">
+      <div className="gjs-grid-column">
+      <Image
+        src="/house_solid.svg"
+        alt="Relow"
+        width={150}
+        height={150}
+        style={{ marginBottom: '2rem' }}
+        priority
+        />
+        <div id="i7nidf" className="text-main-content">Buka aplikasi Relow</div>
+        </div><div className="gjs-grid-column">
+        <Image
+        src="/cart_shopping_solid.svg"
+        alt="Relow"
+        width={150}
+        height={150}
+        style={{ marginBottom: '2rem' }}
+        priority
+        />
+    <div id="is8k9o" className="text-main-content">Pesan order baru</div>
+    </div><div className="gjs-grid-column">
+    <Image
+        src="/list_solid.svg"
+        alt="Relow"
+        width={150}
+        height={150}
+        style={{ marginBottom: '2rem' }}
+        priority
+        />
+      <div id="i2madm" className="text-main-content">Cek status sepatu</div>
+      </div></div><div id="iwaw4c" className="gjs-grid-row">
+        <div id="iauvhi" className="gjs-grid-column"><h2 id="ibx04w" className="gjs-heading">
+        <span id="in98za">Pengalaman mereka</span><br/>bersama kami</h2>
+        <div id="i6tlq1" className="text-main-content">Yuk kita lihat pengalaman dari pengguna jasa kita yang sudah merasakan seberapa mudah<br/>
+        mencuci sepatu mereka dengan jasa Relow. Simak nih beberapa testimoni dari rekan kita!<br/></div>
+        <div className="gjs-grid-row"><div className="gjs-grid-column">
+        <Image
+        src="/yodyod.png"
+        alt="Relow"
+        width={150}
+        height={150}
+        style={{ marginBottom: '2rem', width: '150', height: '150'}}
+        priority
+        /><div id="ilsw14" className="text-main-content">
+            <b id="i2qym3">Yodi</b></div><div id="i9u6o4" className="text-main-content">Ini sangat trendy apalagi<br/>
+            buat anak muda yang selalu mantap fashionnya</div></div><div className="gjs-grid-column">
+            <Image
+        src="/zada.jpg"
+        alt="Relow"
+        width={150}
+        height={150}
+        style={{ marginBottom: '2rem' }}
+        priority
+        /><div id="igfcaj" className="text-main-content">
+        <b id="i6y44y">Zada</b></div><div id="iycjvv" className="text-main-content">
+          Aku sempet bingung kalau<br/>mau cuci sepatu dimana<br/>untung ada aplikasi Relow</div></div>
+          <div className="gjs-grid-column"> 
+          <Image
+        src="/lao.jpg"
+        alt="Relow"
+        width={150}
+        height={150}
+        style={{ marginBottom: '2rem' }}
+        priority
+        />
+          <div id="i3a3yz" className="text-main-content"><b id="ine986">Laode</b></div>
+          <div id="ijmg5d" className="text-main-content">Alhamdulillah setelah Relow saya merasa sangat percaya diri</div></div></div></div></div>
+          <div id="iv3be5" className="gjs-grid-row"><div id="ilriti" className="gjs-grid-column">
+            <div id="id7ol" className="gjs-text-blue">Terms &amp; Conditions</div>
+            <a id="ipjdtn" href="https://grapesjs.com/" className="gjs-link">2023 Relow - All your right are belong to us</a></div></div>
+            </body>
     </>
   )
 }
